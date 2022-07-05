f30d8f9160d5c60a5837a2a705a0d0369ba7fbd8
Matias Mustafa <mustafamatias@hotmail.com>
2022-07-05 20:58:40 -0300

    creaciÃon contador

---
+++ src/components/ItemCount.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+
+const ItemCount = ({stock, initial, onAdd}) => {
+    const [count, setCount] = useState(initial);
+    const incrementar =()=>{
+        setCount(count+1);
+    }
+    const decrementar=()=>{
+        setCount((val)=>(val>0?count-1:count));
+    }
+
+    return (
+        <div className="card bgNav m-auto ">
+            <div className="card2 text-light ">
+                <div>
+                    <button onClick={decrementar} className="btnCount"> - </button>
+                    <span className='fw-bold fs-4'>{count}</span>
+                    <button onClick={incrementar} className="btnCount">+</button>
+                </div>
+                <div>
+                    <button onClick={()=>{
+                        if(count<=stock){
+                            onAdd(`Agregaste ${count} producto/s`);
+                        }else{
+                            onAdd("No hay stock disponible!");
+                        }
+                    }}
+                    className="btnCarrito" ><span>Agregar</span></button>
+                </div>
+            </div>
+        </div>);
+}
+
+export default ItemCount
