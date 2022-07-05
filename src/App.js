2e6404a78dceddaad6435a35d50fcb97ccfbcaa6
Matias Mustafa <mustafamatias@hotmail.com>
2022-07-05 20:57:20 -0300

    creación contador

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import ItemListConteiner from './components/ItemListConteiner';
 
-
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <ItemListConteiner greeting="Bienvenido a tu Store"
-      />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bgNav">
+      <NavBar/>
+      <ItemListConteiner greeting="Bienvenido a tu Store"/>
     </div>
   );
 }
--- src/components/ItemListConteiner.js
+++ src/components/ItemListConteiner.js
@@ -1,6 +1,20 @@
+import ItemCount from "./ItemCount";
+import Swal from 'sweetalert2';
+
 const ItemListConteiner = ({greeting}) =>{
     return (<div className="ItemListConteiner">
         {greeting}
+        <ItemCount stock={5} initial={1} onAdd={(n)=>{
+            Swal.fire({
+            position: 'top-end',
+            background: '#323232',
+            color: '#fff',            
+            icon: 'info',
+            title: n,
+            showConfirmButton: false,
+            timer: 1500
+            })
+        }}/>
     </div>);
 }
 
